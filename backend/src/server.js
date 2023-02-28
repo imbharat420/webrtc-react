b7b9221b049614d2c4b5ed37482dfa553e8ff20c
imbharat420 <imbharat420@gmail.com>
2023-02-28 20:53:00 +0530

    changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,10 @@
-const http = require('http')
-const app = require('./app.js')
+const http = require('http');
+const app = require('./app.js');
 
 const server = http.createServer(app);
 server.listen(8000, () => {
-  const [connectedPort] = server._connectionKey.match(/\d+$/);
-  console.log(`App running on port "${connectedPort}"`);
+   const [connectedPort] = server._connectionKey.match(/\d+$/);
+   console.log(`App running on port "${connectedPort}"`);
 });
 
 module.exports = server;
